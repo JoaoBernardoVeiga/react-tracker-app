15c815d4a635f221c15fa40e3b8cad9db6d577f4
JoaoBernardoVeiga <83598646+JoaoBernardoVeiga@users.noreply.github.com>
2024-02-19 18:33:49 +0000

    JSON server, useEffect hook, About & Footer Components, Routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
 /* eslint-disable react/prop-types */
 import { PropTypes } from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation();
 
-  return (
-    <header className='header'>
-      <h1>{title}</h1>
-      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
-    </header>
-  )
-}
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-  onAdd: PropTypes.func.isRequired,
-  showAdd: PropTypes.bool.isRequired,
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            {location.pathname === '/' && (
+                <Button
+                    color={showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                    onClick={onAdd}
+                />
+            )}
+        </header>
+    );
 };
 
-export default Header
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    onAdd: PropTypes.func.isRequired,
+    showAdd: PropTypes.bool.isRequired,
+};
+
+export default Header;
